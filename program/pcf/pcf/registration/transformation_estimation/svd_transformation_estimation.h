097fc751965e5f0defd0df2525d4fee93c999c2d
Tim Lenertz <timlenertz@gmail.com>
2014-11-13 11:16:21 +0100

    Revert "Revert "icp (generators)""
    This reverts commit a181bcdd761232ecd73bcc2e9c7d81188d1df914.

--- program/pcf/pcf/registration/transformation_estimation/svd_transformation_estimation.h
+++ program/pcf/pcf/registration/transformation_estimation/svd_transformation_estimation.h
@@ -1,25 +1,17 @@
 #ifndef PCF_SVD_TRANSFORMATION_ESTIMATION_H_
 #define PCF_SVD_TRANSFORMATION_ESTIMATION_H_
 
+#include "correlation_matrix_transformation_estimation.h"
 #include <Eigen/Eigen>
 #include <Eigen/Geometry>
-#include "transformation_estimation.h"
 
 namespace pcf {
 
-template<typename Correspondences>
-class svd_transformation_estimation : public transformation_estimation<Correspondences> {
-	using super = transformation_estimation<Correspondences>;
-
-public:
-	explicit svd_transformation_estimation(const Correspondences& cor) :
-		super(cor) { }
-	
+class svd_transformation_estimation : public correlation_matrix_transformation_estimation {
+public:	
 	Eigen::Affine3f operator() () const;
 };
 
 }
-
-#include "svd_transformation_estimation.tcc"
 
 #endif
